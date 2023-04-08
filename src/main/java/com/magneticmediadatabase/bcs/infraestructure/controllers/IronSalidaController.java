197bf6774c4e6f7bac94f05619ee00f52d934d11
camilodev9708 <jclopez41@ucatolica.edu.co>
2023-04-08 04:44:56 -0500

    Merge HU_MG_02 into main V1

--- src/main/java/com/magneticmediadatabase/bcs/infraestructure/controllers/IronSalidaController.java
+++ src/main/java/com/magneticmediadatabase/bcs/infraestructure/controllers/IronSalidaController.java
@@ -1,4 +1,8 @@
 package com.magneticmediadatabase.bcs.infraestructure.controllers;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class IronSalidaController {
+
 }
