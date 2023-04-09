95e84d63889f1ba17807d3d79e7a3988d98c0f6c
camilodev9708 <jclopez41@ucatolica.edu.co>
2023-04-09 01:18:35 -0500

    configuracion en la capa de aplicacion para iron salida

--- src/main/java/com/magneticmediadatabase/bcs/infraestructure/controllers/IronSalidaController.java
+++
@@ -1,8 +0,0 @@
-package com.magneticmediadatabase.bcs.infraestructure.controllers;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class IronSalidaController {
-
-}
