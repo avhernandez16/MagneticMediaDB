962af029078eb34a83ad029f5df892f256ec3d53
camilodev9708 <jclopez41@ucatolica.edu.co>
2023-04-25 19:03:34 -0500

    Implementacion Serializable

--- src/main/java/com/magneticmediadatabase/bcs/domain/model/IronSalida.java
+++ src/main/java/com/magneticmediadatabase/bcs/domain/model/IronSalida.java
@@ -1,101 +1,49 @@
 package com.magneticmediadatabase.bcs.domain.model;
 
 
+import com.magneticmediadatabase.bcs.infraestructure.entity.MedioEntity;
+import com.magneticmediadatabase.bcs.infraestructure.entity.TipoTransporteEntity;
+import com.magneticmediadatabase.bcs.infraestructure.entity.UbicacionFisicaEntity;
+import com.magneticmediadatabase.bcs.infraestructure.entity.UsuarioEntity;
+import lombok.Data;
+
 import java.time.LocalDate;
 
+@Data
 public class IronSalida {
     private Long id;
     private LocalDate fecha_salida;
     private LocalDate fecha_devolucion;
-    private String usuario;
-    private String ubicacion;
-    private String tipo_transporte;
-    private Long numero_remision;
-    private Long codigo;
+    private TipoTransporte tipoTransporte;
+    private TipoTransporteEntity tipoTransporteEntity;
+    private Medio medio;
+    private MedioEntity medioEntity;
+    private Usuario usuario;
+    private UsuarioEntity usuarioEntity;
+    private UbicacionFisica ubicacionFisica;
+    private UbicacionFisicaEntity ubicacionFisicaEntity;
 
-//    Constructores
 
-    public IronSalida (
-            Long id,
-            LocalDate fecha_salida,
-            LocalDate fecha_devolucion,
-            String usuario,
-            String ubicacion,
-            String tipo_transporte,
-            Long numero_remision,
-            Long codigo
-    ) {
+    public IronSalida(Long id, LocalDate fecha_salida, LocalDate fecha_devolucion, TipoTransporte tipoTransporte, Medio medio, Usuario usuario, UbicacionFisica ubicacionFisica) {
         this.id = id;
         this.fecha_salida = fecha_salida;
         this.fecha_devolucion = fecha_devolucion;
+        this.tipoTransporte = tipoTransporte;
+        this.medio = medio;
         this.usuario = usuario;
-        this.ubicacion = ubicacion;
-        this.tipo_transporte = tipo_transporte;
-        this.numero_remision = numero_remision;
-        this.codigo = codigo;
+        this.ubicacionFisica = ubicacionFisica;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
+    public IronSalida(Long id, LocalDate fecha_salida, LocalDate fecha_devolucion, TipoTransporteEntity tipoTransporteEntity, MedioEntity medioEntity, UsuarioEntity usuarioEntity, UbicacionFisicaEntity ubicacionFisicaEntity) {
         this.id = id;
-    }
-
-    public LocalDate getFecha_salida() {
-        return fecha_salida;
-    }
-
-    public void setFecha_salida(LocalDate fecha_salida) {
         this.fecha_salida = fecha_salida;
-    }
-
-    public LocalDate getFecha_devolucion() {
-        return fecha_devolucion;
-    }
-
-    public void setFecha_devolucion(LocalDate fecha_devolucion) {
         this.fecha_devolucion = fecha_devolucion;
+        this.tipoTransporteEntity = tipoTransporteEntity;
+        this.medioEntity = medioEntity;
+        this.usuarioEntity = usuarioEntity;
+        this.ubicacionFisicaEntity = ubicacionFisicaEntity;
     }
 
-    public String getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
-    }
-
-    public String getUbicacion() {
-        return ubicacion;
-    }
-
-    public void setUbicacion(String ubicacion) {
-        this.ubicacion = ubicacion;
-    }
-
-    public String getTipo_transporte() {
-        return tipo_transporte;
-    }
-
-    public void setTipo_transporte(String tipo_transporte) {
-        this.tipo_transporte = tipo_transporte;
-    }
-
-    public Long getNumero_remision() {
-        return numero_remision;
-    }
-
-    public void setNumero_remision(Long numero_remision) {
-        this.numero_remision = numero_remision;
-    }
-
-    public Long getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(Long codigo) {
-        this.codigo = codigo;
+    public IronSalida() {
     }
 }
